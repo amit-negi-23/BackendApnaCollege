ee16faeb106418d13c5c01de729d9e26b023a887
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-03 23:32:54 +0530

    post request

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.urlencoded({extended: true}));  
+app.use(express.json())
 
 app.get('/register', (req, res) => {
     let { user, password } = req.query;
-    res.send(`Standard Get Request. Welcome ${user}!`)
+    res.send(`Standard Get Request. Welcome @${user}!`)
 });
 
 app.post('/register', (req, res) => {
-    res.send("Standard Post Request")
+    let { user, password } = req.body;
+    res.send(`Standard Post Request. Welcome  @${user} !`)
 });
 
 app.listen(port, () => {
