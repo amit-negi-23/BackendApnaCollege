e72e0364b8ec3b20930659dbaa3db351ed42ac8e
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-06 14:01:47 +0530

    Restfull Api

---
+++ Rest_Class/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "apnacollege",
+        content : "I love coding!"
+    },
+    {
+        username: "amitnegi",
+        content : "I like teaching and coding !"
+    },
+    {
+        username: "rohitkashyap",
+        content : "He is a Php developer !"
+    },
+];
+
+app.get('/posts', (req, res)=>{
+    res.render("index.ejs", {posts});
+});
+
+app.listen(port, () => {
+    console.log("Listening to post : 8080");
+});
