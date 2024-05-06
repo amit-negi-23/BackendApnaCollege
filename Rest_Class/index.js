03fc1f1894c9f53b1952886fe93cea67632a7085
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-06 16:32:28 +0530

    indexRoute, new route, redirect

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -13,21 +13,31 @@ app.use(express.static(path.join(__dirname, "public")));
 let posts = [
     {
         username: "apnacollege",
-        content : "I love coding!"
+        content: "I love coding!"
     },
     {
         username: "amitnegi",
-        content : "I like teaching and coding !"
+        content: "I like teaching and coding !"
     },
     {
         username: "rohitkashyap",
-        content : "He is a Php developer !"
+        content: "He is a Php developer !"
     },
 ];
 
-app.get('/posts', (req, res)=>{
-    res.render("index.ejs", {posts});
+app.get('/posts', (req, res) => {
+    res.render("index.ejs", { posts });
 });
+
+app.get('/posts/new', (req, res) => {
+    res.render("new.ejs")
+})
+
+app.post('/posts', (req, res) => {
+    let { username, content } = req.body;
+    posts.push({ username, content })
+    res.redirect("/posts")
+})
 
 app.listen(port, () => {
     console.log("Listening to post : 8080");
