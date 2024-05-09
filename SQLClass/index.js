96dd5cfc10b67ac4e1e773e439b835df917760b6
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-09 23:50:17 +0530

    show users

--- SQLClass/index.js
+++ SQLClass/index.js
@@ -2,6 +2,10 @@ const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -24,14 +28,28 @@ app.get('/', (req, res)=>{
     try {
         connection.query(q, (err, result) => {
             if (err) throw err;
-            console.log(result[0]["count(*)"]);
-            res.send("Success");
+            let count = result[0]["count(*)"];
+            res.render("home.ejs", {count});
         });
     } catch (err) {
         console.log(err);
         res.send("Some error in DB");
     }
     
+});
+
+app.get('/user', (req, res)=>{
+    let q = `SELECT * FROM user`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let users = result;
+            res.render("showusers.ejs", {users});
+        });
+    } catch (err) {
+        console.log(err);
+        res.send("Some error in DB");
+    }
 });
 
 app.listen("8080", ()=>{
