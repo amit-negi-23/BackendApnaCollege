22ab929a439226d75296d245fa275c86a7589db7
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-16 17:15:53 +0530

    model (Chat)

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path  = require("path");
+const Chat = require('./models/chat.js')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -15,6 +16,17 @@ main().then(()=>{
 })
 .catch((err)=>{
     console.log(err)
+})
+
+let chat1 = new Chat({
+    from: "Amit",
+    to: "Sooraj",
+    msg: "Hello , sir",
+    created_at: new Date()
+});
+
+chat1.save().then((res)=>{
+    console.log(res)
 })
 
 app.get('/', (req, res)=>{
